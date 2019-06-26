335ce11802c127763171499b0169fba8ea4c0662
Nicolas <remyblunt_@hotmail.com>
2019-06-26 10:42:49 -0400

    Commit Router Views Servicios

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -11,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue') //Llamada de las vistas
     },
     {
       path: '/about',
@@ -20,6 +20,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/Servicios',
+      name: 'Servicios',
+      component: () => import(/* webpackChunkName: "about" */ './views/Servicios.vue')
+
     }
   ]
 })
