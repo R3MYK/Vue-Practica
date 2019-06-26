123f1981ed2da17f68e0dd8d9da9cb70983d52b3
Nicolas <remyblunt_@hotmail.com>
2019-06-26 11:29:15 -0400

    Commit Rutas dinamicas

--- src/router.js
+++ src/router.js
@@ -7,11 +7,10 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue') //Llamada de las vistas
+      component: () => import( /* webpackChunkName: "home" */ './views/Home.vue') //Llamada de las vistas
     },
     {
       path: '/about',
@@ -19,12 +18,17 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       path: '/Servicios',
       name: 'Servicios',
-      component: () => import(/* webpackChunkName: "about" */ './views/Servicios.vue')
+      component: () => import( /* webpackChunkName: "Servicios" */ './views/Servicios.vue')
+    },
+    {
+      path: '/fotos/:id',
+      name: 'fotos',
+      component: () => import( /* webpackChunkName: "Fotos" */ './views/Fotos.vue')
 
     }
   ]
---
+++ src/views/Fotos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Galeria de Fotografias : </h1>
+    <Foto />
+  </div>
+</template>
+
+<script>
+import  Foto  from "@/components/Fotografia.vue";
+
+export default {
+  name: 'Fotos',
+  components: {
+    Foto
+  }
+}
+</script>
